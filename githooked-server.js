9ab97e89e137cdd957621666027c6aa7fd3067e8
Scott O'Neal <scottmod@gmail.com>
2016-02-06 17:05:12 -0500

    adding listen callback to server.

--- githooked-server.js
+++ githooked-server.js
@@ -1,3 +1,5 @@
 var githooked = require('githooked');
 
-githooked('test', 'exit 1', { secret: process.env.GITHOOKED_SECRET || 'thisismysecret' }).listen(process.env.PORT || 3000);
+githooked('test', 'exit 1', { secret: process.env.GITHOOKED_SECRET || 'thisismysecret' }).listen(process.env.PORT || 3000, function() {
+  console.log('Githooked Test Server listening on port: ', this.address().port);
+});
