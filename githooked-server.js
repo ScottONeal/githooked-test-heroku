c6e4dff90e3977c8c584f6370cf019750034579e
Scott O'Neal <scottmod@gmail.com>
2016-02-06 17:10:15 -0500

    adding error event listening for debugging purposes.

--- githooked-server.js
+++ githooked-server.js
@@ -1,5 +1,10 @@
 var githooked = require('githooked');
 
-githooked('test', 'echo "WEBHOOK RECEIVED"', { secret: process.env.GITHOOKED_SECRET || 'thisismysecret' }).listen(process.env.PORT || 3000, function() {
+var server = githooked('test', 'echo "WEBHOOK RECEIVED"', { secret: process.env.GITHOOKED_SECRET || 'thisismysecret' }).listen(process.env.PORT || 3000, function() {
   console.log('Githooked Test Server listening on port: ', this.address().port);
 });
+
+server.on('error', function(msg) {
+  console.error(msg);
+});
+
