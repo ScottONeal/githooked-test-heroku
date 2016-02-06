4716200df847142cccf80a9fc358e95009025727
Scott O'Neal <scottmod@gmail.com>
2016-02-06 17:27:37 -0500

    changing action to a anon function

--- githooked-server.js
+++ githooked-server.js
@@ -1,14 +1,9 @@
 var githooked = require('githooked');
 
-var server = githooked('refs/heads/master', 'echo "WEBHOOK RECEIVED"', { secret: process.env.GITHOOKED_SECRET || 'thisismysecret' }).listen(process.env.PORT || 3000, function() {
+var server = githooked('refs/heads/master', function() { console.log('WEBHOOK RECEIVED'); }, { secret: process.env.GITHOOKED_SECRET || 'thisismysecret' }).listen(process.env.PORT || 3000, function() {
   console.log('Githooked Test Server listening on port: ', this.address().port);
 });
 
 server.on('error', function(msg) {
   console.error(msg);
 });
-
-server.on('refs/heads/master', function() {
-  console.log('WEB HOOKED RECEIVED!');
-});
-
